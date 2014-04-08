77acfb06f824227705b969ab3450e3b7b9ca6648
William McKenna <spacevacation@gmail.com>
2014-04-08 17:14:57 -0400

    Src-aware & example html
    Modified to fit general/idiosyncratic use

--- plugin/text-to-speech/tts.js
+++ plugin/text-to-speech/tts.js
@@ -32,10 +32,36 @@
             { src: 'plugin/text-to-speech/tts.js', async: true, condition: function() { return !!document.querySelector( '[text-to-speech]' ); } }
         ]
 */
+
+
+// var thisScript = document.currentScript;
+// var scriptSource = thisScript.src;
+    /* stores location of this script, but only works in Chrome, FF, Opera */
+
+var scriptSource = (function(scripts) {
+    var scripts = document.getElementsByTagName('script'),
+        script = scripts[scripts.length - 1];
+
+    if (script.getAttribute.length !== undefined) {
+        return script.src
+    }
+
+    return script.getAttribute('src', -1)
+}());
+
+// var presentPath = scriptSource.substring(0, scriptSource.length - 6);
+    /* removes 6 chars (filename 'tts.js') to give site-specific path */
+
+var presentPath = scriptSource.substring(0, scriptSource.lastIndexOf("/") + 1);
+    /* general trim of script name from src path, keeps the slash */
+
 function loadScript(url, callback) {
+ 
+
 
     var script = document.createElement('script')
     script.type = 'text/javascript';
+    
 
     if (script.readyState) { //IE
         script.onreadystatechange = function () {
@@ -55,8 +81,9 @@ function loadScript(url, callback) {
     document.getElementsByTagName('head')[0].appendChild(script);
 }
 
+
 //sound manager used for any browser that doesn't support HTML 5 audio tag
-loadScript('plugin/text-to-speech/soundmanager2-jsmin.js', function () {
+loadScript(presentPath + 'soundmanager2-jsmin.js', function () {
     //initialise soundManager
     soundManager.setup({
         url: '/',
@@ -67,7 +94,7 @@ loadScript('plugin/text-to-speech/soundmanager2-jsmin.js', function () {
     });
 });
 
-loadScript('plugin/text-to-speech/google-tts.js', function () {
+loadScript(presentPath + 'google-tts.js', function () {
     Reveal.addEventListener('slidechanged', function (event) {
         nextindexh = event.indexh;
         var sentence = document.getElementsByTagName('section')[nextindexh].getAttribute('text-to-speech');
